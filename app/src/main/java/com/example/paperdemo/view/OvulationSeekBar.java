085f22869ed004dd0392543c79bfa8ee9e9a60ff
xiongyl <xiongyl@ikangtai.com>
2019-12-23 20:02:56 +0800

    修改试纸显示

--- app/src/main/java/com/example/paperdemo/view/OvulationSeekBar.java
+++ app/src/main/java/com/example/paperdemo/view/OvulationSeekBar.java
@@ -26,6 +26,16 @@ public class OvulationSeekBar extends LinearLayout  {
     public static final int POS_6 = 71;
     public static final int POS_7 = 85;
     public static final int POS_8 = 100;
+
+    private final int VALUE_1 = 0;
+    private final int VALUE_2 = 5;
+    private final int VALUE_3 = 10;
+    private final int VALUE_4 = 15;
+    private final int VALUE_5 = 20;
+    private final int VALUE_6 = 25;
+    private final int VALUE_7 = 45;
+    private final int VALUE_8 = 65;
+
     private TextView seekbarTitle;
     private MySeekBar mySeekBar;
     private LinearLayout seekbar1, seekbar2, seekbar3, seekbar4, seekbar5, seekbar6,seekbar7,seekbar8;
@@ -55,7 +65,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(0);
+                mySeekBar.setProgress(POS_1);
                 if (callback != null) {
                     callback.changeResult(1);
                 }
@@ -64,7 +74,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar2.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(15);
+                mySeekBar.setProgress(POS_2);
                 if (callback != null) {
                     callback.changeResult(5);
                 }
@@ -73,7 +83,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar3.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(29);
+                mySeekBar.setProgress(POS_3);
                 if (callback != null) {
                     callback.changeResult(10);
                 }
@@ -82,7 +92,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar4.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(43);
+                mySeekBar.setProgress(POS_4);
                 if (callback != null) {
                     callback.changeResult(15);
                 }
@@ -91,7 +101,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar5.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(57);
+                mySeekBar.setProgress(POS_5);
                 if (callback != null) {
                     callback.changeResult(20);
                 }
@@ -100,7 +110,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar6.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(71);
+                mySeekBar.setProgress(POS_6);
                 if (callback != null) {
                     callback.changeResult(25);
                 }
@@ -109,7 +119,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar7.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(85);
+                mySeekBar.setProgress(POS_7);
                 if (callback != null) {
                     callback.changeResult(45);
                 }
@@ -118,7 +128,7 @@ public class OvulationSeekBar extends LinearLayout  {
         seekbar8.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                mySeekBar.setProgress(100);
+                mySeekBar.setProgress(POS_8);
                 if (callback != null) {
                     callback.changeResult(65);
                 }
@@ -140,24 +150,30 @@ public class OvulationSeekBar extends LinearLayout  {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 int seekProgress = seekBar.getProgress();
                 if (callback != null) {
-                    if (seekProgress < 11) {
-                        setSeekBarStatus(0);
-                        callback.changeResult(0);
-                    } else if (seekProgress >= 11 && seekProgress < 31) {
-                        setSeekBarStatus(5);
-                        callback.changeResult(5);
-                    } else if (seekProgress >= 31 && seekProgress < 51) {
-                        setSeekBarStatus(10);
-                        callback.changeResult(10);
-                    } else if (seekProgress >= 51 && seekProgress < 71) {
-                        setSeekBarStatus(25);
-                        callback.changeResult(25);
-                    } else if (seekProgress >= 71 && seekProgress < 91) {
-                        setSeekBarStatus(45);
-                        callback.changeResult(45);
-                    } else if (seekProgress >= 91) {
-                        setSeekBarStatus(65);
-                        callback.changeResult(65);
+                    if (seekProgress < POS_2) {
+                        setSeekBarStatus(POS_1);
+                        callback.changeResult(VALUE_1);
+                    } else if (seekProgress >= POS_2 && seekProgress < POS_3) {
+                        setSeekBarStatus(POS_2);
+                        callback.changeResult(VALUE_2);
+                    } else if (seekProgress >= POS_3 && seekProgress < POS_4) {
+                        setSeekBarStatus(POS_3);
+                        callback.changeResult(VALUE_3);
+                    } else if (seekProgress >= POS_4 && seekProgress < POS_5) {
+                        setSeekBarStatus(POS_4);
+                        callback.changeResult(VALUE_4);
+                    } else if (seekProgress >= POS_5 && seekProgress < POS_6) {
+                        setSeekBarStatus(POS_5);
+                        callback.changeResult(VALUE_5);
+                    } else if (seekProgress >= POS_6 && seekProgress < POS_7) {
+                        setSeekBarStatus(POS_6);
+                        callback.changeResult(VALUE_6);
+                    } else if (seekProgress >= POS_7 && seekProgress < POS_8) {
+                        setSeekBarStatus(POS_7);
+                        callback.changeResult(VALUE_7);
+                    } else if (seekProgress >= POS_8) {
+                        setSeekBarStatus(POS_8);
+                        callback.changeResult(VALUE_8);
                     }
                 }
             }
@@ -167,23 +183,29 @@ public class OvulationSeekBar extends LinearLayout  {
 
     public void setSeekBarStatus(int result) {
         switch (result) {
-            case 0:
-                mySeekBar.setProgress(0);
+            case VALUE_1:
+                mySeekBar.setProgress(POS_1);
                 break;
-            case 5:
-                mySeekBar.setProgress(20);
+            case VALUE_2:
+                mySeekBar.setProgress(POS_2);
                 break;
-            case 10:
-                mySeekBar.setProgress(40);
+            case VALUE_3:
+                mySeekBar.setProgress(POS_3);
                 break;
-            case 25:
-                mySeekBar.setProgress(60);
+            case VALUE_4:
+                mySeekBar.setProgress(POS_4);
                 break;
-            case 45:
-                mySeekBar.setProgress(80);
+            case VALUE_5:
+                mySeekBar.setProgress(POS_5);
                 break;
-            case 65:
-                mySeekBar.setProgress(100);
+            case VALUE_6:
+                mySeekBar.setProgress(POS_6);
+                break;
+            case VALUE_7:
+                mySeekBar.setProgress(POS_7);
+                break;
+            case VALUE_8:
+                mySeekBar.setProgress(POS_8);
                 break;
         }
     }
